99aec48c9093c2ec7e294e121edb1fd9dcffdd84
dongyoon <biisairo415@gmail.com>
2022-06-06 23:45:49 +0900

    기본 알고리즘 완성, flag, win lose 구현필요

--- mine.c
+++ mine.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+
+#define MINE 10
+#define ROW 20
+#define COL 20
 
 int value_in_array(int *array, int num);
 void create_map(char ***map);
@@ -11,89 +16,97 @@ int *create_mine();
 int is_mine(char ***map, int mine_cor[][2], int r, int c);
 int count_mine(int range[], int mine_cor[][2]);
 void display_count_mine(char ***map, int count_mine, int r, int c);
-void check_side(char ***map, int range[], int side);
 void find_mine_in_range(char ***map, int range[], int mine_cor[][2]);
 int find_mine(char ***map, int mine_cor[][2], int r, int c);
+void show_mine(char ***map, int mine_cor[][2]);
 
-
-
-int main() {
+int main()
+{
     // 게임판 생성
-    char ***map = malloc(sizeof(char **) * 10);
-    for (int i = 0; i < 10; i++) {
-        map[i] = malloc(sizeof(char *) * 15);
+    char ***map = malloc(sizeof(char **) * ROW);
+    for (int i = 0; i < ROW; i++)
+    {
+        map[i] = malloc(sizeof(char *) * COL);
     }
     create_map(map);
     print_map(map);
 
     // 지뢰 데이터 생성
     int *mine = create_mine();
-    int mine_cor[20][2];
-    for (int i = 0; i < 20; i++) {
-        int r = mine[i] / 15;
-        int c = mine[i] % 15;
+    int mine_cor[MINE][2];
+    for (int i = 0; i < MINE; i++)
+    {
+        int r = mine[i] / COL;
+        int c = mine[i] % COL;
         mine_cor[i][0] = r;
         mine_cor[i][1] = c;
     }
-    //생성된 지뢰 확인
-    //for (int i = 0; i < 20; i++) {
-    //    printf("%d %d\n", mine_cor[i][0], mine_cor[i][1]);
-    //}
+
     // 지뢰 좌표 입력 후 할당된 데이터 해제
     free(mine);
 
     int check = 1;
-    while(check) {
+    while (check)
+    {
         int a, b;
         scanf("%d %d", &a, &b);
-        if (a < 0 || a > 9 || b < 0 || b > 14) continue;
+        if (a < 0 || a > (ROW - 1) || b < 0 || b > (COL - 1))
+            continue;
 
         check = is_mine(map, mine_cor, a, b);
         find_mine(map, mine_cor, a, b);
         print_map(map);
     }
 
-
-
-
-
-
-
-
-
-
-
     free_map(map);
     return 0;
 }
 
 // 해당 변수가 배열에 있는지 확인, 지뢰 생성 중 중복 방지로 만듬
-int value_in_array(int *array, int num) {
-    for (int i = 0; i < 20; i++) {
-        if (array[i] == num) return 1;
+int value_in_array(int *array, int num)
+{
+    for (int i = 0; i < MINE; i++)
+    {
+        if (array[i] == num)
+            return 1;
     }
     return 0;
 }
 
 // 맴 생성, 각 좌표에 유니코드 삽입
-void create_map(char ***map) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 15; j++) {
+void create_map(char ***map)
+{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
+        {
             map[i][j] = " ░░ ";
         }
     }
 }
 
 // 맵 출력
-void print_map(char ***map) {
+void print_map(char ***map)
+{
     system("clear");
-    printf("  ");
-    for (int i = 0; i < 10; i++) printf("%d   ", i);
-    for (int i = 10; i < 15; i++) printf("%d  ", i);
+    printf("   ");
+    for (int i = 0; i < 10; i++)
+        printf("%d   ", i);
+    for (int i = 10; i < ROW; i++)
+        printf("%d  ", i);
     printf("\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%d", i);
-        for (int j = 0; j < 15; j++) {
+    for (int i = 0; i < ROW; i++)
+    {
+        if (i < 10)
+        {
+            printf("%d ", i);
+        }
+        else if (i >= 10)
+        {
+            printf("%d", i);
+        }
+        for (int j = 0; j < COL; j++)
+        {
             printf("%s", map[i][j]);
         }
         printf("\n");
@@ -101,22 +114,29 @@ void print_map(char ***map) {
 }
 
 // 맵에 할당된 동적 메모리 해제
-void free_map(char ***map) {
-    for (int i = 0; i < 10; i++) {
+void free_map(char ***map)
+{
+    for (int i = 0; i < ROW; i++)
+    {
         free(map[i]);
     }
     free(map);
 }
 
 // 난수 생성기로 지뢰 생성
-int *create_mine() {
+int *create_mine()
+{
     int *mine = malloc(sizeof(int));
     srand((unsigned int)time(NULL));
-    for (int i = 0; i < 20; i++) {
-        int random = rand() % 151;
-        if (!value_in_array(mine, random)) {
+    for (int i = 0; i < MINE; i++)
+    {
+        int random = rand() % ((ROW * COL) + 1);
+        if (!value_in_array(mine, random))
+        {
             mine[i] = random;
-        } else {
+        }
+        else
+        {
             i--;
         }
     }
@@ -124,31 +144,43 @@ int *create_mine() {
 }
 
 // 지뢰 찾기 알고리즘
-int find_mine(char ***map, int mine_cor[][2], int r, int c) {
+int find_mine(char ***map, int mine_cor[][2], int r, int c)
+{
     // 지뢰가 없다면
-    if (is_mine(map, mine_cor, r, c)) {
+    if (is_mine(map, mine_cor, r, c))
+    {
         // 탐지 범위를 구하고
         int range[4];
         int r1, r2, c1, c2;
         // r 축의 탐지 범위
-        if (r == 0) {
+        if (r == 0)
+        {
             r1 = r;
             r2 = r + 1;
-        } else if (r == 9) {
+        }
+        else if (r == (ROW - 1))
+        {
             r1 = r - 1;
             r2 = r;
-        } else {
+        }
+        else
+        {
             r1 = r - 1;
             r2 = r + 1;
         }
         // c 축의 탐지 범위
-        if (c == 0) {
+        if (c == 0)
+        {
             c1 = c;
             c2 = c + 1;
-        } else if (r == 14) {
+        }
+        else if (c == (COL - 1))
+        {
             c1 = c - 1;
             c2 = c;
-        } else {
+        }
+        else
+        {
             c1 = c - 1;
             c2 = c + 1;
         }
@@ -160,18 +192,31 @@ int find_mine(char ***map, int mine_cor[][2], int r, int c) {
         int count = count_mine(range, mine_cor);
         // 선택한 칸에 탐지한 지뢰의 숫자를 대입
         display_count_mine(map, count, r, c);
-//        // 이외의 탐지한 칸은 임시 체크를 해 준 뒤
-//        check_side(map, range, count);
-        // ░░  인 칸만 추가로 다시 지뢰를 찾는다
-        find_mine_in_range(map, range, mine_cor);
+
+        if (count == 0)
+        {
+            for (int i = r1; i <= r2; i++)
+            {
+                for (int j = c1; j <= c2; j++)
+                {
+                    if (strcmp(map[i][j], " ░░ ") == 0)
+                    {
+                        find_mine(map, mine_cor, i, j);
+                    }
+                }
+            }
+        }
     }
     return 1;
 }
 
 // 선택 한 값의 지뢰의 유무 판별
-int is_mine(char ***map, int mine_cor[][2], int r, int c) {
-    for (int i = 0; i < 20; i++) {
-        if (mine_cor[i][0] == r && mine_cor[i][1] == c) {
+int is_mine(char ***map, int mine_cor[][2], int r, int c)
+{
+    for (int i = 0; i < MINE; i++)
+    {
+        if (mine_cor[i][0] == r && mine_cor[i][1] == c)
+        {
             map[r][c] = " ▓▓ ";
             return 0;
         }
@@ -180,19 +225,23 @@ int is_mine(char ***map, int mine_cor[][2], int r, int c) {
 }
 
 // 탐지 범위 내의 지뢰 갯수 구하기
-int count_mine(int range[], int mine_cor[][2]) {
+int count_mine(int range[], int mine_cor[][2])
+{
     int r1 = range[0];
     int r2 = range[1];
     int c1 = range[2];
     int c2 = range[3];
 
     int side = 0;
-    for (int i = r1; i <= r2; i++) {
-        for (int j = c1; j <= c2; j++) {
-            for (int k = 0; k < 20; k++) {
-                if (mine_cor[k][0] == i && mine_cor[k][1] == j) {
+    for (int i = r1; i <= r2; i++)
+    {
+        for (int j = c1; j <= c2; j++)
+        {
+            for (int k = 0; k < MINE; k++)
+            {
+                if (mine_cor[k][0] == i && mine_cor[k][1] == j)
+                {
                     side++;
-
                 }
             }
         }
@@ -201,67 +250,82 @@ int count_mine(int range[], int mine_cor[][2]) {
 }
 
 // 지뢰의 갯수를 맵에 저장
-void display_count_mine(char ***map, int side, int r, int c) {
-    switch (side) {
-        case 0:
-            map[r][c] = "    ";
-            break;
-        case 1:
-            map[r][c] = " 1  ";
-            break;
-        case 2:
-            map[r][c] = " 2  ";
-            break;
-        case 3:
-            map[r][c] = " 3  ";
-            break;
-        case 4:
-            map[r][c] = " 4  ";
-            break;
-        case 5:
-            map[r][c] = " 5  ";
-            break;
-        case 6:
-            map[r][c] = " 6  ";
-            break;
-        case 7:
-            map[r][c] = " 7  ";
-            break;
-        case 8:
-            map[r][c] = " 8  ";
-            break;
-    }
-}
-
-// 근접한 칸에 임시 체크
-void check_side(char ***map, int range[], int side) {
-    int r1 = range[0];
-    int r2 = range[1];
-    int c1 = range[2];
-    int c2 = range[3];
-
-    if (side == 0) {
-        for (int i = r1; i <= r2; i++) {
-            for (int j = c1; j <= c2; j++) {
-                if (strcmp(map[i][j], " ░░ ") == 0) {
-                    map[i][j] = " 00 ";
-                }
-            }
-        }
+void display_count_mine(char ***map, int side, int r, int c)
+{
+    switch (side)
+    {
+    case 0:
+        map[r][c] = "    ";
+        break;
+    case 1:
+        map[r][c] = " 1  ";
+        break;
+    case 2:
+        map[r][c] = " 2  ";
+        break;
+    case 3:
+        map[r][c] = " 3  ";
+        break;
+    case 4:
+        map[r][c] = " 4  ";
+        break;
+    case 5:
+        map[r][c] = " 5  ";
+        break;
+    case 6:
+        map[r][c] = " 6  ";
+        break;
+    case 7:
+        map[r][c] = " 7  ";
+        break;
+    case 8:
+        map[r][c] = " 8  ";
+        break;
     }
 }
 
 // 범위 내에서 지뢰 찾기 알고리즘을 돌리되, 추가로 찾는건  ░░  인 칸에만 적용
-void find_mine_in_range(char ***map, int range[], int mine_cor[][2]) {
+void find_mine_in_range(char ***map, int range[], int mine_cor[][2])
+{
     int r1 = range[0];
     int r2 = range[1];
     int c1 = range[2];
     int c2 = range[3];
-    for (int i = r1; i <= r2; i++) {
-        for (int j = c1; j <= c2; j++) {
-            if (strcmp(map[i][j], " ░░ ") == 0) {
+    for (int i = r1; i <= r2; i++)
+    {
+        for (int j = c1; j <= c2; j++)
+        {
+            if (strcmp(map[i][j], " ░░ ") == 0)
+            {
                 find_mine(map, mine_cor, i, j);
             }
         }
+    }
+}
+
+void show_mine(char ***map, int mine_cor[][2])
+{
+    system("clear");
+    printf("  ");
+    for (int i = 0; i < ROW; i++)
+        printf("%d   ", i);
+    for (int i = ROW; i < COL; i++)
+        printf("%d  ", i);
+    printf("\n");
+    for (int i = 0; i < ROW; i++)
+    {
+        printf("%d", i);
+        for (int j = 0; j < COL; j++)
+        {
+            if (!is_mine(map, mine_cor, i, j))
+            {
+                printf(" ▓▓ ");
+            }
+            else
+            {
+                printf("%s", map[i][j]);
+            }
+        }
+        printf("\n");
     }
 }
